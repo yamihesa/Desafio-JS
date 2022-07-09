9eb570942a56204179f18d02125c5430e5b1da23
yamilahesain <yamihesa@hotmail.com>
2022-07-09 13:39:23 -0300

    modificando

--- Documentos/JavaScrip Desafio/JS/script.js
+++ Documentos/JavaScrip Desafio/JS/script.js
@@ -1,28 +1,26 @@
-let nombre = prompt("Ingrese su nombre");
-alert("Bienvenido a mi página " + nombre);
 
-let edad = parseInt(prompt("ingrese su edad:"));
-if(edad >= 18){
+
+for(let i = 1 ; i < 10 ; i++){
+
+    if(i == 1){
+       
+}
+    let nombre = prompt("Ingrese su nombre");
+    alert("Bienvenido a mi página " + nombre);
+
+
+    let edad = parseInt(prompt("ingrese su edad:"));
+    if(edad >= 18){
     alert("Sos mayor de edad!");
 }
-else{
+    else{
     alert("No sos mayor de edad!");
 }
 
-/*-----------------*/
+    let dniIngresado = prompt("Ingrese su DNI:");
 
-for(let i = 1 ; i < 50 ; i++){
-
-    if(i == 1){
-
-        alert("Apurate, esta es la ultima entrada")
-
-    }
-
-    let dniIngresado = prompt("Ingrese su DNI:")
-
-    alert("DNI " + dniIngresado + "." + "\nSu entrada es la N:" + i)
-
+    alert("DNI " + dniIngresado + "." + "\nSu entrada es la N:" + i);
+    alert("Gracias por su visita")
+    break;
 }
 
-alert("Nos quedamos sin entradas por hoy")
