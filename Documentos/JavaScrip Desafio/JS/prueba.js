a59cd84dc40ef0ebb9321a138a4f7866f8c320fd
yamilahesain <yamihesa@hotmail.com>
2022-07-13 08:42:13 -0300

    ciclos

--- Documentos/JavaScrip Desafio/JS/prueba.js
+++ Documentos/JavaScrip Desafio/JS/prueba.js
@@ -3,33 +3,35 @@ for (let i = 1; i <= 5; i++) {
 let nombre = prompt("Ingrese su nombre");
 let apellido = prompt("Ingrese su apellido");
 
-alert("Bienvenido " + nombre + " " + apellido );
+    alert("Bienvenido " + nombre + " " + apellido );
 
 /*Pedir Edad */
 let edad = parseInt(prompt("Ingrese su edad"));
     
-if(edad >=18){
-    alert("Sos mayor de edad!!");
-}else{
-    alert("No sos mayor de edad!!");
-}
+    if(edad >=18){
+        alert("Sos mayor de edad!!");
+    }else{
+        alert("No sos mayor de edad!!");
+    }
 
 /*For*/
     
-    let ingresarMail = prompt("Ingresar Mail");
-    let ingresarDni = parseInt(prompt("Ingrese su DNI"));
+let ingresarMail = prompt("Ingresar Mail");
+let ingresarDni = parseInt(prompt("Ingrese su DNI"));
     alert(" Entrada N° " + i);
     
     
 /*While*/
     
-    let comentario = prompt("¿Desea dejar un comentario?");
+let comentario = prompt("¿Desea dejar un comentario?");
+
     while(comentario != "no"){
         alert("Comentario ingresado: "+ comentario);
         comentario = prompt("¿Deseas agregar otro comentario?");
     }
+    alert("Gracias por dejarnos tu comentario!!!")
 }
-alert("Se terminaron las entradas, hasta la proxima")
+    alert("Se terminaron las entradas, hasta la proxima")
     
 
 
